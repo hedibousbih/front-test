fec4da2f5e2c7ec0fcee19ff48a93b1d252db381
hedibousbih <80211666+hedibousbih@users.noreply.github.com>
2024-04-18 01:18:52 +0100

    bideyet

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,12 +5,32 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { DivHomeComponent } from './div-home/div-home.component';
+import { SporthomeComponent } from './sporthome/sporthome.component';
+import { StadehomeComponent } from './stadehome/stadehome.component';
+import { AbonnementhomeComponent } from './abonnementhome/abonnementhome.component';
+import { BlogComponent } from './blog/blog.component';
+import { BlogdetailsComponent } from './blogdetails/blogdetails.component';
+import { NavbarconnectedComponent } from './navbarconnected/navbarconnected.component';
+import { LoginComponent } from './login/login.component';
+import { CommentsComponent } from './comments/comments.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    DivHomeComponent,
+    SporthomeComponent,
+    StadehomeComponent,
+    AbonnementhomeComponent,
+    BlogComponent,
+    BlogdetailsComponent,
+    NavbarconnectedComponent,
+    LoginComponent,
+    CommentsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ front/src/app/sporthome/sporthome.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sporthome',
+  templateUrl: './sporthome.component.html',
+  styleUrls: ['./sporthome.component.css']
+})
+export class SporthomeComponent {
+
+}
